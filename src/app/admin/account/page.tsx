d02b2bd675f82c6903485a6d9327f2e17912fda4
StartingFrom101 <nguyen.bb.minh@gmail.com>
2024-01-15 10:05:11 +0700

    Account Table

--- src/app/admin/account/page.tsx
+++ src/app/admin/account/page.tsx
@@ -1,9 +1,73 @@
 'use client'
 
-import {Typography} from "antd";
+import {Table, Tag, Typography} from "antd";
+import moment from "moment";
 
 export default function AccountPage() {
-	return <div>
-		<Typography.Text>this is account page</Typography.Text>
-	</div>
+	return (	
+<div className="container mx-auto">
+<Table
+              bordered={true}
+              columns={[
+                {
+                  title: 'ID',
+                  dataIndex: 'entranceId',
+                  key: 'entranceId',
+                },
+                {
+                  title: 'Username',
+                  dataIndex: 'entranceId',
+                  key: 'entranceId',
+                },
+                {
+                  title: 'Email',
+                  dataIndex: 'entranceId',
+                  key: 'entranceId',
+                },
+                {
+                  title: 'First Name',
+                  dataIndex: 'entranceId',
+                  key: 'entranceId',
+                },
+                {
+                  title: 'Last Name',
+                  dataIndex: 'entranceId',
+                  key: 'entranceId',
+                },
+                {
+                  title: 'Date of Birth',
+                  dataIndex: 'entranceId',
+                  key: 'entranceId',
+                },
+                {
+                  title: 'Gender',
+                  dataIndex: 'entranceId',
+                  key: 'entranceId',
+									render: (type: 0 | 1) => (
+                    <>{type === 1 ? <span>Male</span> : <span>Female</span>}</>
+                  ),
+                },
+                {
+                  title: 'Phone',
+                  dataIndex: 'entranceId',
+                  key: 'entranceId',
+                },
+                {
+                  title: 'Address',
+                  dataIndex: 'entranceId',
+                  key: 'entranceId',
+                },
+                {
+                  title: 'Role',
+                  dataIndex: 'type',
+                  key: 'type',
+                  render: (type: 0 | 1) => (
+                    <>{type === 1 ? <Tag color={'red'}>Admin</Tag> : <Tag color={'green'}>Customer</Tag>}</>
+                  ),
+                },
+              ]}
+              pagination={false}
+            />	</div>
+	)
+
 }
